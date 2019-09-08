ea7cab6fcd72e4ba8ef34959c996dddb609d61bf
Max Frei <max.frei@uni-due.de>
2019-09-08 23:41:37 +0200

    Cleaned up code.

--- blender_utilities.py
+++ blender_utilities.py
@@ -152,19 +152,19 @@ def enable_all_rendering_devices():
     scene = bpy.context.scene
     scene.cycles.device = "GPU"
 
-    prefs = bpy.context.preferences
-    cprefs = prefs.addons["cycles"].preferences
+    preferences = bpy.context.preferences
+    cycles_preferences = preferences.addons["cycles"].preferences
 
     # Attempt to set GPU device types if available
     for compute_device_type in ["CUDA", "OPENCL", "NONE"]:
         try:
-            cprefs.compute_device_type = compute_device_type
+            cycles_preferences.compute_device_type = compute_device_type
             break
         except TypeError:
             pass
 
     # Enable all CPU and GPU devices
-    for device in cprefs.devices:
+    for device in cycles_preferences.devices:
         print(device)
         device.use = True
 
