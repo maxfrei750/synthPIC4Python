c1b9ecfbf0faeb5e88bfbfb1e32b270eda764610
Max Frei <max.frei@uni-due.de>
2019-09-08 21:56:50 +0200

    Made it work, just to see that volumetric absorption is not supported. Switched to blender.

--- test/test_pymesh.py
+++ test/test_pymesh.py
@@ -1,44 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
-import pymesh as pm
-
-# This import registers the 3D projection, but is otherwise unused.
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
+import trimesh
+import pyrender
 import matplotlib.pyplot as plt
 
-
-# In[2]:
-
-
-A = pm.generate_icosphere(10,np.array([0,0,0]),refinement_order=3)
-
-
-# In[3]:
-
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-x = A.vertices[:,0]
-y = A.vertices[:,1]
-z = A.vertices[:,2]
-
-print(y.shape)
-
-# Plot the surface
-ax.plot_trisurf(x, y, z, color='b')
-
+fuze_trimesh = trimesh.load('examples/models/fuze.obj')
+mesh = pyrender.Mesh.from_trimesh(fuze_trimesh)
+scene = pyrender.Scene()
+scene.add(mesh)
+camera = pyrender.PerspectiveCamera(yfov=np.pi / 3.0, aspectRatio=1.0)
+s = np.sqrt(2)/2
+camera_pose = np.array([
+   [0.0, -s,   s,   0.3],
+   [1.0,  0.0, 0.0, 0.0],
+   [0.0,  s,   s,   0.35],
+   [0.0,  0.0, 0.0, 1.0],
+])
+scene.add(camera, pose=camera_pose)
+light = pyrender.SpotLight(color=np.ones(3), intensity=3.0,
+                           innerConeAngle=np.pi/16.0,
+                           outerConeAngle=np.pi/6.0)
+scene.add(light, pose=camera_pose)
+r = pyrender.OffscreenRenderer(400, 400)
+color, depth = r.render(scene)
+plt.figure()
+plt.subplot(1,2,1)
+plt.axis('off')
+plt.imshow(color)
+plt.subplot(1,2,2)
+plt.axis('off')
+plt.imshow(depth, cmap=plt.cm.gray_r)
 plt.show()
-
-
-# In[ ]:
-
-
-
-
