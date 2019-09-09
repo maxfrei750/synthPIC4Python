06b4e007ad2ce273f0f7971647a3f0bcdcbc592f
Max Frei <max.frei@uni-due.de>
2019-09-09 15:16:56 +0200

    Initial commit.

---
+++ install_dependencies.bat
@@ -0,0 +1,89 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 4
+:: Automatically check & get admin rights
+:: see "https://stackoverflow.com/a/12264592/1016343" for description
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS
+
+:init
+setlocal DisableDelayedExpansion
+set cmdInvoke=1
+set winSysFolder=System32
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+
+if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+goto ExecElevation
+
+:InvokeCmd
+ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+"%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+
+set blender_directory=C:\Program Files\Blender Foundation\Blender\2.80
+c:
+cd "%blender_directory%\python\bin\" 2> nul
+
+if ERRORLEVEL 1 (
+    echo [31mCouldn't find compatible blender installation. Please check if the supplied path is correct.[0m
+    echo. 
+    pause
+    exit /b 1
+)
+
+python.exe -m ensurepip
+python.exe -m pip install -U pip
+
+echo.
+echo [32mUninstalling the numpy version that come with blender, because it is buggy.[0m 
+python.exe -m pip uninstall --yes numpy
+
+echo.
+echo [32mDeleting numpy remains.[0m 
+rmdir /S /Q %blender_directory%\python\lib\site-packages\numpy 2> nul
+
+echo.
+echo [32mInstalling dependencies.[0m 
+python.exe -m pip install numpy
+python.exe -m pip install trimesh
+
+if ERRORLEVEL 0 (
+    echo.
+	echo [32mSetup complete.[0m
+	echo. 
+)
+
+pause
