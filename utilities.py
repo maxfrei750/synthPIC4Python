789e558e4c9f8f061e42a4625ff675f6263fb690
Max Frei <max.frei@uni-due.de>
2019-10-03 19:36:52 +0200

    Introduce Timer wrapper.

--- utilities.py
+++ utilities.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import time
 
 def execute(cmd):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
@@ -14,3 +14,16 @@ def execute(cmd):
 def execute_and_print(cmd):
     for line in execute(cmd):
         print(line)
+
+
+class Timer(object):
+    def __init__(self, name=None):
+        self.name = name
+
+    def __enter__(self):
+        self.tstart = time.time()
+
+    def __exit__(self, type, value, traceback):
+        if self.name:
+            print('[%s]' % self.name,)
+        print('Elapsed: %s' % (time.time() - self.tstart))
