a7b2fa8722ad6a90e89fe1c337cc530f54e352fc
Max Frei <max.frei@uni-due.de>
2019-10-03 19:38:56 +0200

    Fix issue that caused slow down over time.

--- recipes/sopat_catalyst.py
+++ recipes/sopat_catalyst.py
@@ -4,8 +4,6 @@ import sys
 import numpy as np
 import random
 
-import time
-
 C = bpy.context
 D = bpy.data
 R = C.scene.render
@@ -16,6 +14,8 @@ if root_dir not in sys.path:
 
 import blender.particles
 import blender.scene
+
+from utilities import Timer
 
 # # Force reload in case you edit the source after you first start the blender session.
 # import importlib
@@ -29,14 +29,16 @@ blender.scene.apply_default_settings()
 resolution = (1032, 825)
 blender.scene.set_resolution(resolution)
 
-
 primitive_path_light = os.path.join(root_dir, "primitives", "sopat_catalyst", "light.blend")
-primitive_light = blender.particles.load_primitive(primitive_path_light)
+primitive_path_dark = os.path.join(root_dir, "primitives", "sopat_catalyst", "dark.blend")
 
-n_images = 100
+n_images = 20
 
 for image_id in range(n_images):
-    t = time.time()
+    blender.scene.save_state()
+
+    primitive_dark = blender.particles.load_primitive(primitive_path_dark)
+    primitive_light = blender.particles.load_primitive(primitive_path_light)
 
     # Ensure reproducibility of the psd.
     random.seed(image_id)
@@ -46,14 +48,14 @@ for image_id in range(n_images):
     n = 200
     d_g = 50
     sigma_g = 1.6
-    particles_dark = generate_lognormal_fraction(primitive_dark, name, n, d_g, sigma_g)
+    particles_dark = blender.particles.generate_lognormal_fraction(primitive_dark, name, n, d_g, sigma_g)
 
     # Create fraction 2: light particles
     name = "light"
     n = 25
     d_g = 50
     sigma_g = 1.6
-    particles_light = generate_lognormal_fraction(primitive_light, name, n, d_g, sigma_g)
+    particles_light = blender.particles.generate_lognormal_fraction(primitive_light, name, n, d_g, sigma_g)
 
     # Combine fractions.
     particles = particles_dark+particles_light
@@ -63,7 +65,7 @@ for image_id in range(n_images):
     lower_space_boundaries_xyz = (-resolution[0]/2, -resolution[1]/2, -10)
     upper_space_boundaries_xyz = (resolution[0]/2, resolution[1]/2, 10)
     damping = 1
-    collision_shape = "box"
+    collision_shape = "sphere"
 
     blender.particles.place_randomly(particles,
                                      lower_space_boundaries_xyz,
@@ -80,7 +82,6 @@ for image_id in range(n_images):
     output_file_path = os.path.join(root_dir, "output", "sopat", output_file_name)
     blender.scene.render_to_file(output_file_path)
 
-    # Delete all particles.
-    blender.particles.delete(particles)
+    blender.scene.reset_state()
 
-    print(time.time()-t)
+blender.scene.clear_state()
