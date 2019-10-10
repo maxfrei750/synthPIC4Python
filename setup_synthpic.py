89731b9b66fecb70966a3218293845e94adf7cfd
Max Frei <max.frei@uni-due.de>
2019-10-10 18:06:52 +0200

    Fixed numpy installation.

--- setup_synthpic.py
+++ setup_synthpic.py
@@ -95,6 +95,10 @@ def install_dependencies():
     execute_and_print([blender_python_executable_path, "-m", "ensurepip"])
     execute_and_print(
         [blender_python_executable_path, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"])
+
+    numpy_folder = os.path.join(get_blender_python_folder(), "lib", "python3.7", "site-packages", "numpy")
+    shutil.rmtree(numpy_folder) 
+
     execute_and_print([blender_python_executable_path, "-m", "pip", "install", "--requirement", requirement_file_path])
 
     print("Successfully installed python and dependencies.")
